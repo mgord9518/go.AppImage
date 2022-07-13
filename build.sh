6faeff1502730aa58d7ebcfebcaf23effb073276
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-07-12 21:38:30 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -180,6 +180,7 @@ cd "$tempDir"
 
 # Remove stuff not needed for runnung
 rm -r "AppDir/usr/test" "AppDir/usr/doc" "AppDir/usr/pkg/linux"*
+rm -r "AppDir/usr/pkg/tool/linux_amd64"
 mv "AppDir/usr/bin/linux_arm64/"* "AppDir/usr/bin"
 strip -s "AppDir/usr/bin/"* "AppDir/usr/pkg/tool/"*/*
 
