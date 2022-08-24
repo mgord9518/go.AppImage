a200ab12104324f1c19dd21528ef7716c08b9007
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-08-24 00:49:53 -0600

    Create build_in_chroot.sh

---
+++ build_in_chroot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sudo apt install qemu-user-static
+
+mkdir -p chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
+
+# Move QEMU to the chroot directory
+cp /usr/bin/qemu-aarch64-static upper/usr/bin
+
+# Mount up the chroot
+sudo mount -t squashfs "bionic-server-cloudimg-$1.squashfs" sfsmnt
+sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
+
+sudo mount -o bind /proc chrootdir/proc/
+#sudo mount -o bind /dev chrootdir/dev/
+sudo mount --rbind /run/systemd chrootdir/run/systemd
+
+# Everything below will be run inside the chroot
+cat << EOF | sudo chroot chrootdir /bin/bash
+sudo apt update
+sudo apt install squashfs-tools squashfuse librsvg2-bin musl musl-tools
+sudo snap install go --classic
+
+wget https://raw.githubusercontent.com/mgord9518/go.AppImage/main/build.sh
+sh build.sh
+EOF
