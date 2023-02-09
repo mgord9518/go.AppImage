35859f1735573f1ffc241b80e7b590abaeea1327
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2023-02-09 00:40:38 -0700

    Update build_in_chroot.sh

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -8,7 +8,7 @@ mkdir -p chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work up
 cp /usr/bin/qemu-aarch64-static upper/usr/bin
 
 # Mount up the chroot
-sudo mount -t squashfs "focal-server-cloudimg-$1.squashfs" sfsmnt
+sudo mount -t squashfs *"-server-cloudimg-$1.squashfs" sfsmnt
 sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
 
 sudo mount -o bind /proc chrootdir/proc/
@@ -19,12 +19,10 @@ sudo mount --rbind /run/systemd chrootdir/run/systemd
 #####################################################################################
 cat << EOF | sudo chroot chrootdir /bin/bash
 sudo apt update
-sudo apt install -y squashfs-tools squashfuse librsvg2-bin musl musl-tools zip
+sudo apt install -y squashfs-tools squashfuse librsvg2-bin musl musl-tools zip golang
 
 #wget -O /usr/bin/go $(curl -q https://api.github.com/repos/mgord9518/go.AppImage/releases | grep $(uname -m) | grep Go | grep shImg | grep browser_download_url | cut -d'"' -f4 | head -n1)
 #chmod +x /usr/bin/go
-
-sudo snap install go --classic
 
 wget https://raw.githubusercontent.com/mgord9518/go.AppImage/main/build.sh
 sh build.sh
